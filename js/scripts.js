d8f2274fdb935d4562efa844abd5e2c0d0579b98
Ben Kiggen and Luke Vandekieft <luke@>
2018-08-09 14:31:37 -0700

    finalize js, minor css changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
-  var age = parseInt(prompt("What is your age?"));
-  console.log(age);
-  if (age) {
-    if (age >= 18) {
-       $("p.voting").toggle();
-    } else {
-      $("p.underage").toggle();
-    }
-    console.log("true");
-  } else {
-    alert ("Please enter a valid age");
-    location.reload();
-    console.log("false");
-  }
+  $("form#triangle").submit(function(event){
+    event.preventDefault();
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
 
+
+    if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2){
+      alert("I know triangles and you sir are no triangle!");
+      location.reload();
+    } else {
+      if (side1 === side2 && side1 === side3 && side2===side3){
+        $("div.result").html("<h3>Equilateral</h3>");
+      } else if (side1 != side2 && side1 != side3 && side2 != side3) {
+        $("div.result").html("<h3>Scalene</h3>");
+      } else {
+        $("div.result").html("<h3>Isosceles</h3>");
+      };
+
+    }
+
+
+
+  });
 });
